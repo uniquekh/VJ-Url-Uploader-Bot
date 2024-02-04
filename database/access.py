937f30295adf03272aa8b3acbd2d005942adb647
Unique <mujeem000@gmail.com>
2024-02-04 17:47:32 +0530

    Update access.py

--- database/access.py
+++ database/access.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @criminal
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @Unique8272
 
 from config import Config
 from database.database import Database
